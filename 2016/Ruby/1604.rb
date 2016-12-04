8320ecdc99bb40c0143132b31bb44586a156225e
Steve Darveau <steve@darvs.net>
2016-12-04 15:37:26 -0500

    2016: - Both problems of Day 4 are now solved.

--- 2016/Ruby/1604.rb
+++ 2016/Ruby/1604.rb
@@ -4,30 +4,43 @@
 def AoC1604(input_file)
 
   total = 0
+  decrypted_list = []
 
   File.open(input_file).readlines.each do |line|
 
-    *words, sector, checksum = line.strip.split(/[\[,\],\-]/)
-    str = words.join('')
+    *words, sector, checksum = line.strip.split(/[\[\],\-]/)
+    str = words.join
 
     occurrences = str.each_char.map{|char| [char, str.scan(char).count]}.to_h.sort_by{|char, frequency| [-frequency, char]}
 
-    calculated_checksum = occurrences.map{|pair| pair[0]}.take(5).join('')
+    calculated_checksum = occurrences.map{|pair| pair[0]}.take(5).join
 
     ok = (calculated_checksum == checksum)
 
-    if ok then total += sector.to_i end
+    if ok
+      total += sector.to_i
 
-    puts("#{if ok then "OK " else "NOT" end} String: #{words.join('-')} Sector: #{sector} Checksum: #{checksum} Occurrences: #{occurrences} Calculated Checksum: #{calculated_checksum}")
+      decrypted_list.push([sector, words.map{|word| word.each_char.map{|char| (((char.ord - 'a'.ord + sector.to_i) % 26) + 'a'.ord).chr}.join}.join(' ')])
+    end
+
+
+    puts("#{'%3s' % if ok then 'OK' else 'NOT' end} String: #{words.join('-')} Sector: #{sector} Checksum: #{checksum} Occurrences: #{occurrences} Calculated Checksum: #{calculated_checksum}")
 
   end
 
   puts('')
   puts("Total Correct Checkum: #{total}")
 
+  puts('-----------------')
+  puts('Decryption')
+  puts('-----------------')
+
+  decrypted_list.map{|sector, decrypted| puts("#{sector} | #{decrypted}")}
+
 end
 
 
 
 #AoC1604('1604.test.input')
+#AoC1604('1604.test2.input')
 AoC1604('1604.real.input')
