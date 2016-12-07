f044c39fefc2c30ca4e030d1fc5b3c9f5467807a
Steve Darveau <steve@darvs.net>
2016-12-07 16:17:57 -0500

    2016: - Day 6 modified to answer both puzzles of the day.

--- 2016/Elixir/1606.exs
+++ 2016/Elixir/1606.exs
@@ -1,10 +1,10 @@
 defmodule AoC1606 do
 
-  def run(file) do
+  def run(file, puzzle, function) do
 
     IO.puts "Starting time: " <> DateTime.to_string(DateTime.utc_now)
 
-    File.stream!(file, [:utf8])
+    rez = File.stream!(file, [:utf8])
       |> Stream.map(&(String.strip/1))
       |> Stream.map(&(String.codepoints(&1)))
       |> Stream.flat_map(&(Enum.zip(0..Enum.count(&1), &1)))
@@ -13,10 +13,12 @@ defmodule AoC1606 do
       |> Map.to_list
       |> IO.inspect
       |> Enum.map(fn {_,v} -> Enum.map(Enum.dedup(Enum.sort(v)), fn(x) -> {x, Enum.count(v, fn(y) -> y == x end)} end) end)
-      |> Enum.map(&(elem(Enum.max_by(&1, fn(x) -> elem(x,1) end),0)))
+      |> Enum.map(&(elem(function.(&1, fn(x) -> elem(x,1) end),0)))
       |> Enum.join
-      |> IO.inspect
 
+      IO.puts("-------------------------------------------------")
+      IO.puts("Answer for #{puzzle} :: #{rez}")
+      IO.puts("-------------------------------------------------")
 
     IO.puts "Ending time: " <> DateTime.to_string(DateTime.utc_now)
 
@@ -25,4 +27,6 @@ end
 
 
 # AoC1606.run("1606.test.input")
-AoC1606.run("1606.real.input")
+
+AoC1606.run("1606.real.input", "Puzzle #1", &(Enum.max_by/2))
+AoC1606.run("1606.real.input", "Puzzle #2", &(Enum.min_by/2))
