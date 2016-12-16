40bd46321e51d50d09e281705ff36b4a1c0f9fad
Steve Darveau <steve@darvs.net>
2016-12-16 14:41:57 -0500

    2016: - Day 14: Add solution to Puzzle #2

--- 2016/Elixir/aoc1614/lib/aoc1614.ex
+++ 2016/Elixir/aoc1614/lib/aoc1614.ex
@@ -7,44 +7,52 @@ defmodule AoC1614 do
     end
   end
 
-  def md5(n, salt) do
-    str = salt <> to_string(n)
+  def md5_compute(str, stretching) do
+    md5 = String.downcase(Base.encode16(:crypto.hash(:md5, str)))
 
-    String.downcase(Base.encode16(:crypto.hash(:md5, str)))
+    case stretching do
+      0 -> md5
+      _ -> md5_compute(md5, stretching-1)
+    end
+  end
+
+
+  def md5_parse(n, salt, stretching) do
+    md5_compute(salt <> to_string(n), stretching)
       |> (fn x -> [ n, Regex.run(~r/([a-f0-9])\1\1/, x), Regex.run(~r/([a-f0-9])\1\1\1\1/, x) ] end).()
       |> (fn [n, r3, r5] -> [n, hd_tl_not_nil(r3), hd_tl_not_nil(r5)] end).()
   end
 
-  def iter(verified, _, _, _) when length(verified) == 64 do
+  def iter(verified, _, _, _, _) when length(verified) == 64 do
     [first | _] = verified
     [n, _, _] = first
     n
   end
 
-  def iter(verified, lookahead, n, salt) when length(lookahead) == 0 do
-    iter(verified, [md5(n, salt)], n, salt)
+  def iter(verified, lookahead, n, salt, stretching) when length(lookahead) == 0 do
+    iter(verified, [md5_parse(n, salt, stretching)], n, salt, stretching)
   end
 
 
-  def iter(verified, lookahead, n, salt) when length(lookahead) < 1001 do
+  def iter(verified, lookahead, n, salt, stretching) when length(lookahead) < 1001 do
 
     i = hd(hd(lookahead))
 
     new_list =  (n+1 .. i+1000)
-                  |> Enum.map(&md5(&1, salt))
+                  |> Enum.map(&md5_parse(&1, salt, stretching))
 
-    iter(verified, lookahead ++ new_list, i+1000, salt)
+    iter(verified, lookahead ++ new_list, i+1000, salt, stretching)
   end
 
-  def iter(verified, lookahead, n, salt) when tl(hd(lookahead)) == [nil, nil] do
+  def iter(verified, lookahead, n, salt, stretching) when tl(hd(lookahead)) == [nil, nil] do
 
     # IO.inspect(["TRYING", tl(lookahead)])
 
-    iter(verified, tl(lookahead), n, salt)
+    iter(verified, tl(lookahead), n, salt, stretching)
   end
 
 
-  def iter(verified, lookahead, n, salt) do
+  def iter(verified, lookahead, n, salt, stretching) do
     # IO.puts("up to n: #{n}, len= #{length(lookahead)}")
 
     [first | rest] = lookahead
@@ -62,16 +70,16 @@ defmodule AoC1614 do
 
     # IO.inspect(["VERIFIED:", new_verified])
 
-    iter(new_verified, rest, n, salt)
+    iter(new_verified, rest, n, salt, stretching)
   end
 
 
 
-  def run(salt) do
+  def run(salt, stretching \\ 0) do
 
     IO.puts "Starting time: " <> DateTime.to_string(DateTime.utc_now)
 
-    x = iter([], [], 0, salt)
+    x = iter([], [], 0, salt, stretching)
 
     IO.puts("Answer for #{salt} is: #{x}" )
 
--- 2016/Elixir/aoc1614/test/aoc1614_test.exs
+++ 2016/Elixir/aoc1614/test/aoc1614_test.exs
@@ -2,11 +2,17 @@ defmodule AoC1614Test do
   use ExUnit.Case
   doctest AoC1614
 
-  test "MD5" do
-    IO.puts("------------------")
-    IO.puts(" MD5")
-    IO.puts("------------------")
-    assert AoC1614.md5(816, "abc") == [816, "e", "e"]
+  test "MD5 Compute" do
+    assert String.contains?(AoC1614.md5_compute("abc18", 0), "cc38887a5")
+  end
+
+  test "MD5 Stretching" do
+    assert AoC1614.md5_compute("abc0", 2016) == "a107ff634856bb300138cac6568c0f24"
+  end
+
+
+  test "MD5 Parse" do
+    assert AoC1614.md5_parse(816, "abc", 0) == [816, "e", "e"]
   end
 
   test "Example" do
@@ -23,4 +29,10 @@ defmodule AoC1614Test do
     assert AoC1614.run("qzyelonm") == 15168
   end
 
+  test "Puzzle #2" do
+    IO.puts("------------------")
+    IO.puts(" Puzzle #2")
+    IO.puts("------------------")
+    AoC1614.run("qzyelonm", 2016) == 20864
+  end
 end
