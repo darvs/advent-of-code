8be2fa1551ed3c29796bb6d1bbfc95c39909df93
Steve Darveau <steve@darvs.net>
2017-12-10 19:27:51 -0500

    2017: - Proper solution to Day 7 of AoC 2017 [Clean]

--- 2017/1707.rb/lib/1707.rb
+++ 2017/1707.rb/lib/1707.rb
@@ -45,14 +45,14 @@ def unbalanced(programs, program_name, error=0)
 
 	if pgm[:supported].empty? then
 		# pgm is a leaf
+		# The error has to be here.
 		return pgm[:weight]-error
+
 	else
 		supported_weight_list = pgm[:supported].map{|sup| [programs[sup][:name], programs[sup][:total_weight]]}
-		puts "SUPPORTED_WEIGHT_LIST:: #{supported_weight_list}"
 		weight_occ = supported_weight_list
 			.transpose[1].each_with_object(Hash.new(0)) {|weight, count| count[weight] += 1}
 			.sort{|a,b| a[1]<=>b[1]}
-		puts "weights:: #{weight_occ}"
 		
 		if weight_occ.length == 1 then
 			# all descendents are balanced, the error is here.
@@ -64,43 +64,7 @@ def unbalanced(programs, program_name, error=0)
 			error = weight_occ[0][0] - weight_occ[1][0]
 			error_pgm = supported_weight_list.select{|sup| sup[1] == weight_occ[0][0]}[0][0]
 
-			puts "ERROR:: #{error} ERROR_PGM:: #{error_pgm}"
-
 			return unbalanced(programs, error_pgm, error)
 		end
-
-
-
-
-
 	end
-end
-
-def unbalanced1(programs, program_name)
-	pgm = programs[program_name]
-
-	supported = pgm[:supported]
-	supported_weight_list = supported.empty? ? [{leaf: 0}] : supported.map{|sup| {sup => weight(programs, sup)}}
-	weights = supported_weight_list.map{|sup| sup.values[0]}
-	weight_count = Hash.new 0
-	weights.map{|w| weight_count[w] += 1}
-
-	correct = 0
-	error = 0
-
-	weight_count.map{|x| if x[1] > 1 then correct=x[0] else error=x[0] end}
-
-	puts "LIST:: #{supported_weight_list} WEIGHTS:: #{weights} WEIGHT_COUNT:: #{weight_count} UNIQ:: #{weight_count.keys.uniq} CORRECT:: #{correct} ERROR: #{error}"
-
-	supported_weight_list.each{|sup| puts "SUP: #{sup}"; if error != 0 and sup.values[0] == error then puts "SUP.KEYS[0]:: #{sup.keys[0]}"; programs[sup.keys[0]][:balanced] = programs[sup.keys[0]][:weight] + (correct - error) end} 
-	
-	#pgm[:balanced] = if error != 0 then pgm[:weight] + (correct - error) else 0 end
-	supported_weight = weights.reduce(:+) 
-	pgm[:total_weight] = pgm[:weight] + supported_weight
-
-	return pgm[:total_weight]
-end
-
-def unbalanced0(programs)
-	puts "UNBALANCED:: #{programs.select {|k,v| v.has_key?(:balanced)}}"
 end
