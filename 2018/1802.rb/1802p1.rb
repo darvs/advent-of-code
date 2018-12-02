e63849fd8cf7abac7a8ab32153b0ed570beee997
Steve Darveau <steve@darvs.net>
2018-12-02 03:36:24 -0500

    2018: - Cleanup of 1802p1

--- 2018/1802.rb/1802p1.rb
+++ 2018/1802.rb/1802p1.rb
@@ -1,10 +1,7 @@
 #!/usr/bin/env ruby
 require 'set'
 h=Hash.new(0)
-has_2=Set.new()
-has_3=Set.new()
+has=Hash.new(nil)
 File.readlines('input').map(&:strip).each{|s| s.each_char.each{|c| h[s+"."+c] += 1}}
-puts h
-h.select{|_,x| [2].include? x}.each{|z,_| puts z; has_2.add(z.split('.')[0])}
-h.select{|_,x| [3].include? x}.each{|z,_| puts z; has_3.add(z.split('.')[0])}
-puts "#{has_2.count}+#{has_3.count}=\n#{has_2.count*has_3.count}"
+h.select{|_,x| [2,3].include? x}.each{|z,v| has[v].nil? ? has[v]=Set[z.split('.')[0]] : has[v].add(z.split('.')[0])}
+puts "#{has[2].count}*#{has[3].count}=\n#{has[2].count*has[3].count}"
