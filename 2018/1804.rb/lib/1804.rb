5c4055481831cfbe1fcc35863e417157d41cdf85
Steve Darveau <steve@darvs.net>
2018-12-04 18:58:20 -0500

    2018: - Final cleanup for Day 4 of AoC 2018

--- 2018/1804.rb/lib/1804.rb
+++ 2018/1804.rb/lib/1804.rb
@@ -14,7 +14,7 @@ def parse(line, schedule, context)
     # The guard was asleep in all the minutes between the last time he fell asleep and the minute before the current minute
 
     for m in context[:fell_asleep_at] .. ((minute - 1) % 60)
-      (schedule[m])[context[:guard]] += 1
+      (schedule[context[:guard]])[m] += 1
     end
 
   end
@@ -23,35 +23,28 @@ end
 
 def exec_file(filename)
 
-  # schedule is an array of minute, in each minute a hash where k=guard_number, v=number of times that guard was asleep at that minute
-  schedule = Array.new(60){Hash.new(0)}
+  # guard_minute_times is the schedule formatted as {guard=>{minute=>times_asleep}}]
+  guard_minute_times = Hash.new{|h,k| h[k] = Hash.new(0)}
 
   # context will keep the current guard at the minute they last fell asleep
   context = Hash.new
 
   File.open(File.join('data', filename)).map(&:strip).select{|line| !line.empty?}
-    .sort.each{|line| parse(line, schedule, context)}
+    .sort.each{|line| parse(line, guard_minute_times, context)}
 
-  minutes_asleep = Hash.new(0)
-  guard_minute_times = Hash.new{|h,k| h[k] = Hash.new}
+  guard_most_frequently_asleep = guard_minute_times.map{|guard, minute_times|
+    [guard, minute_times.map{|_,times| times}.sum]
+  }.max_by{|guard, times| times}.first
 
-  schedule.each_with_index{|minute, index|
-    minute.each{|guard,times_asleep|
-      minutes_asleep[guard] += times_asleep
-
-      guard_minute_times[guard][index] = times_asleep
-    }
-  }
-  guard_most_minutes_asleep = minutes_asleep.max_by{|k,v| v}.first
-
-  guard_most_minutes_asleep_most_asleep_at = guard_minute_times[guard_most_minutes_asleep].max_by{|k,v| v}.first
+  guard_most_frequently_asleep_most_frequently_asleep_at = guard_minute_times[guard_most_frequently_asleep].max_by{|_minutes, times| times}.first
 
   guard_most_frequently_asleep_on_same_minute = ([:guard, :minute].zip(
     guard_minute_times.map{|guard, minute_times|
       minute, times = minute_times.max_by{|_, _times| _times}
       [times, [guard, minute]]
-    }.max_by{|times, guard_minute| times}.each{|x| puts "!!! #{x} !!!"}.last)
+    }.max_by{|times, guard_minute| times}.last)
   ).to_h
 
- return guard_most_minutes_asleep, guard_most_minutes_asleep_most_asleep_at, guard_most_frequently_asleep_on_same_minute
+ return guard_most_frequently_asleep, guard_most_frequently_asleep_most_frequently_asleep_at, guard_most_frequently_asleep_on_same_minute
+
 end
