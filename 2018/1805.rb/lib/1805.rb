b6e771b2c9275a3261d8d991a76ec432648ba368
Steve Darveau <steve@darvs.net>
2018-12-07 20:47:02 -0500

    2018: - Modified 1805.rb to make it more likable to Rubocop

--- 2018/1805.rb/lib/1805.rb
+++ 2018/1805.rb/lib/1805.rb
@@ -1,43 +1,39 @@
-def parse(line, skip=nil)
-
+def parse(line, skip = nil)
   head = []
   tail = line.chars
 
-  while tail.length > 0 do
-
-    if !skip.nil? && skip.casecmp?(tail[0]) then
+  until tail.empty?
+    if !skip.nil? && skip.casecmp?(tail[0])
       tail.shift
 
-    elsif head.length == 0 then
+    elsif head.empty?
       head = [tail.shift]
 
-    elsif head[-1].swapcase == tail[0] then
+    elsif head[-1].swapcase == tail[0]
       head.pop
       tail.shift
 
     else
       head.concat([tail.shift])
-
     end
-
   end
 
-  return head.join
+  head.join
 end
 
 def exec_file(filename)
-
-  polymer = File.open(File.join('data', filename)).map(&:strip).select{|line| !line.empty?}.first
+  polymer = File.open(File.join('data', filename)).map(&:strip)
+                .reject(&:empty?).first
 
   stable = parse(polymer)
 
-  filtering = [:unit, :len, :result].zip(('a'..'z').map{|c|
+  filtering = %i[unit len result].zip(('a'..'z').map{|c|
     result = parse(polymer, c)
     [c, result.length, result]
-  }.min_by{|_, len, _| len}).to_h
+  }
+    .min_by{|_, len, _| len}).to_h
 
   #puts "filtering #{filtering}"
 
-  return stable, filtering
-
+  [stable, filtering]
 end
