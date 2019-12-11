c6c428ea790597a96cfa4d55b2c879ed88c69ba1
Steve Darveau <steve@darvs.net>
2019-12-11 10:28:03 -0500

    - Slight clean-up for Day 11 of AoC 2019

--- 2019/1911.rb/lib/1911.rb
+++ 2019/1911.rb/lib/1911.rb
@@ -15,14 +15,13 @@ class PaintingRobot
     @panels[key(0, 0)] = initial_color
 
     loop do
-      computer.add_input(@panels[key(x, y)])
-      computer.run
+      computer.add_input(@panels[key(x, y)]).run
       break if computer.state == :terminated
 
       @panels[key(x, y)], turn = computer.output.split(',').map(&:to_i)
-      turn = -1 if turn.zero?
 
-      orientation = (orientation + turn) % DIRECTIONS.length
+      orientation += turn.zero? ? -1 : 1
+      orientation %= DIRECTIONS.length
 
       x += DIRECTIONS[orientation][0]
       y += DIRECTIONS[orientation][1]
@@ -41,13 +40,12 @@ class PaintingRobot
     val = @panels.map{|k, _| k}.each_with_object(Hash.new(Set.new)){|k, hash|
       hash[:x] += [k[0]]
       hash[:y] += [k[1]]
-      #hash
     }
     colors = ' #'
-    output = val[:y].max.downto(val[:y].min).reduce([]){|list, y|
-      list + [(val[:x].min..val[:x].max).reduce(''){|str, x|
-        str + colors[@panels[key(x, y)]]
-      }]
+    output = val[:y].max.downto(val[:y].min).map{|y|
+      (val[:x].min..val[:x].max).map{|x|
+        colors[@panels[key(x, y)]]
+      }.reduce(&:+)
     }
 
     output.each{|line| p line}
