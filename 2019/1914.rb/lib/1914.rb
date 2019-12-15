c3b009d784c5c8b0760ed12851f06659bf1dbb72
Steve Darveau <steve@darvs.net>
2019-12-15 00:50:33 -0500

    - Cleaner version of Part 1 of Day 14

--- 2019/1914.rb/lib/1914.rb
+++ 2019/1914.rb/lib/1914.rb
@@ -7,8 +7,7 @@ class Stoichiometry
   attr_reader :ore
 
   def initialize(recipes)
-    p [:initialize, recipes]
-    @ore = create(recipes, [[:FUEL, 1]])
+    @ore = transform(recipes, [[:FUEL, 1]])
   end
 
   def self.from_file(filename)
@@ -16,8 +15,6 @@ class Stoichiometry
       .map(&:strip)
       .each_with_object({}){|l, h| parse(h, l)})
   end
-
-  
 
   def self.parse(hash, line)
     parsed = line.split(' => ').map{|s|
@@ -28,47 +25,36 @@ class Stoichiometry
     hash[result] = { qty: qty, recipe: parsed.flatten(1) }
   end
 
-  def create(recipes, objectives, leftovers = Hash.new(0), ore = 0)
-    return ore if objectives.empty?
+  def scale(recipe, qty)
+    return recipe if recipe[:qty] >= qty
 
-    p [:create, objectives, leftovers, ore]
+    factor = (qty.to_f / recipe[:qty]).ceil
 
-    goal, qty = objectives.shift
-    p [goal, qty]
-
-    if goal == :ORE
-      return create(recipes, objectives, leftovers, ore + qty)
-    end
-
-    left = leftovers[goal]
-    leftovers[goal] = [leftovers[goal] - qty, 0].max
-    qty = [qty - left, 0].max
-
-    p [:goal, goal, :qty, qty, :objectives, objectives, :leftovers, leftovers, :ore, ore]
-
-    if qty.zero?
-      return create(recipes, objectives, leftovers, ore)
-    end
-
-    # Dity trick to make a deep copy
-    recipe = Marshal.load(Marshal.dump(recipes[goal]))
-
-    p [:recipe, recipe, :qty, qty]
-
-    # The recipe produces too little
-    if qty > recipe[:qty]
-      factor = (qty.to_f/recipe[:qty]).ceil
-      recipe[:qty] *= factor
-      recipe[:recipe].map!{|k, v| [k, v * factor]}
-      p [:factor, factor, :new_recipe, recipe]
-    end
-
-    # The recipe produces too much
-    if recipe[:qty] > qty
-      leftovers[goal] = recipe[:qty] - qty
-    end
-
-    return create(recipes, recipe[:recipe] + objectives, leftovers, ore)
+    { qty: recipe[:qty] * factor,
+      recipe: recipe[:recipe].map{|k, v| [k, v * factor]} }
   end
 
+  def transform(recipes, objectives, leftovers = Hash.new(0), ore = 0)
+    return ore if objectives.empty?
+
+    goal, qty = objectives.shift
+
+    return transform(recipes, objectives, leftovers, ore + qty) if goal == :ORE
+
+    # See if we already have what we need in the leftovers
+    from_leftovers = [leftovers[goal], qty].min
+    leftovers[goal] -= from_leftovers
+    qty -= from_leftovers
+
+    # Did we fulfill everything ftom the leftovers?
+    return transform(recipes, objectives, leftovers, ore) if qty.zero?
+
+    # Scale the recipe to make enough to cover qty
+    scaled = scale(recipes[goal], qty)
+
+    # If we produced too much there will be some leftovers
+    leftovers[goal] += scaled[:qty] - qty if scaled[:qty] > qty
+
+    transform(recipes, scaled[:recipe] + objectives, leftovers, ore)
+  end
 end
