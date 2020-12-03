d54424c29f7eb652ba79b14caff22e9b7f795515
Steve Darveau <steve@darvs.net>
2020-12-03 14:59:43 -0500

    - A more idiomatic version of Day 3 of Advent of Code 2020

--- 2020/2003.rb/lib/2003.rb
+++ 2020/2003.rb/lib/2003.rb
@@ -12,29 +12,13 @@ class Toboggan
   end
 
   def run(part)
-    case part
-    when 1
-      slopes = [[3, 1]]
-    when 2
-      slopes = [[1, 1], [3, 1], [5, 1], [7, 1], [1, 2]]
-    end
+    slopes = [[[3, 1]], [[1, 1], [3, 1], [5, 1], [7, 1], [1, 2]]][part - 1]
 
-    mul = 1
-
-    slopes.map{|sl|
-      trees = 0
-      x = 0
-      y = 0
-      while y < @list.length - 1
-        y += sl[1]
-        x += sl[0]
-        #puts @list[y]
-        #puts @list[y][x]
-        trees += 1 if @list[y][x % @list[y].length] == '#'
-      end
-      trees
+    slopes.map{|x, y|
+      (0..@list.length - 1).step(y).select.with_index{|line_num, index|
+        line = @list[line_num]
+        line[(x * index) % line.length] == '#'
+      }.count
     }.reduce(&:*)
-
   end
-
 end
