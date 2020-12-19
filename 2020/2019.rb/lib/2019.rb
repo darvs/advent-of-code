a837faba3fc357c45c1c5a80a1bdab62acaeb981
Steve Darveau <steve@darvs.net>
2020-12-19 10:01:05 -0500

    - Improved Day 19 of Advent of Code 2020. Still failing part 2

--- 2020/2019.rb/lib/2019.rb
+++ 2020/2019.rb/lib/2019.rb
@@ -47,116 +47,44 @@ class Grammar
 
   def check_rule(num, msg)
     rule = @rules[num]
-    #msg = @msg.dup
+    @msg = msg.dup
 
-    puts "num #{num} rule #{rule}"
+    puts "rule #{rule}"
     if rule.length == 1
 
       # A character
       if rule.flatten.first.is_a? String
-        puts "a string @msg #{@msg} rule.f.f #{rule.flatten.first}"
+        #puts "num #{num} rule #{rule} string @msg #{@msg} rule.f.f #{rule.flatten.first}"
         if @msg[0] == rule.flatten.first
-          puts "true"
+          puts "#{num} TRUE rule #{rule} string @msg #{@msg} rule.f.f #{rule.flatten.first}"
           @msg = @msg[1..-1]
           return true
         else
-          puts "false"
+          puts "#{num} FALSE rule #{rule} string @msg #{@msg} rule.f.f #{rule.flatten.first}"
           return false
         end
       else
         # A concatenation
-        puts "concat #{rule.flatten}"
-        return rule.flatten.all?{|r| check_rule(r, @msg)}
+        puts "#{num} concat #{rule.first}"
+        try = rule.first.all?{|r| check_rule(r, @msg)}
+        @msg = msg.dup if !try
+        return try
       end
 
     else
 
       # several parts separated with "|"
       return rule.any?{|r|
+        puts "#{num} any #{rule} r #{r}"
         @msg = msg.dup
-        r.flatten.all?{|r2| check_rule(r2, @msg)}
+        try = r.all?{|r2| check_rule(r2, @msg)}
+        @msg = msg.dup if !try
+        try
       }
     end
   end
 
   def check_all_messages
     @messages.map{|msg| check_message(msg)}.sum
-  end
-end
-
-# Equation
-class Equation
-  def initialize
-    @list = []
-  end
-
-  def parse(str_or_list)
-    @list = str_or_list.is_a?(Array) ? str_or_list : str_or_list.gsub('(', '( ').gsub(')', ' )').split(' ')
-    self
-  end
-
-  def self.with_priority(part)
-    case part
-    when 1
-      EquationWithNoPriority.new
-    when 2
-      EquationWithPriorityPlus.new
-    end
-  end
-
-  # solve will solve the parens only and use the subclasses'
-  # reduce methods to solve the rest according to the priority
-  def solve
-    while (rparen = @list.find_index(')'))
-      lparen = rparen - @list[0..rparen - 1].reverse.find_index('(') - 1
-      sub = @list[lparen + 1..rparen - 1]
-      @list[lparen..rparen] = Equation.with_priority(@part).parse(sub).solve
-    end
-
-    reduce
-  end
-
-  def next_token
-    @list.slice!(0)
-  end
-end
-
-# An Equation where + and * have equal priority
-class EquationWithNoPriority < Equation
-  def initialize
-    @part = 1
-    super
-  end
-
-  def reduce
-    while @list.length > 1
-      @list[0, 3] = case @list[1]
-                    when '+'
-                      @list[0].to_i + @list[2].to_i
-                    when '*'
-                      @list[0].to_i * @list[2].to_i
-                    end
-    end
-    @list.first
-  end
-end
-
-# An Equation where + has priority
-class EquationWithPriorityPlus < Equation
-  def initialize
-    @part = 2
-    super
-  end
-
-  def reduce
-    while (plus = @list.find_index('+'))
-      @list[plus - 1, 3] = @list[plus - 1].to_i + @list[plus + 1].to_i
-    end
-
-    while (times = @list.find_index('*'))
-      @list[times - 1, 3] = @list[times - 1].to_i * @list[times + 1].to_i
-    end
-
-    @list.first
   end
 end
