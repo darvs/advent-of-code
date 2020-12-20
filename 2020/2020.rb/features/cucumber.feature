d40f919442ea0e1b9541ec3440c81fe486a61833
Steve Darveau <steve@darvs.net>
2020-12-20 12:06:34 -0500

    - Added naive solution to Part 1 of Day 20 of Advent of Code 2020

--- 2020/2020.rb/features/cucumber.feature
+++ 2020/2020.rb/features/cucumber.feature
@@ -6,6 +6,10 @@ Feature: Day 19: Jurassic Jigsaw
 		Given the puzzle in file "2020.smol.in"
 		Then the multiplication of the corners would be 20899048083289
 
+	#Scenario: Part 1 
+		#Given the puzzle in file "2020.in"
+		#Then the multiplication of the corners would be 0
+
 
 
 
--- 2020/2020.rb/lib/2020.rb
+++ 2020/2020.rb/lib/2020.rb
@@ -5,6 +5,7 @@ class Puzzle
   def initialize(lines)
     @tiles = {}
     @edges = {}
+    @flipped = {}
     parse(lines)
   end
 
@@ -14,15 +15,15 @@ class Puzzle
   end
 
   def parse(lines)
-    puts lines.to_s
+    #puts lines.to_s
     until lines.empty? || lines.first.empty?
       m = /\ATile (?<tile>\d+):\Z/.match(lines.first)
-      tile = m[:tile]
+      tile = m[:tile].to_i
       lines.slice!(0)
 
       picture = []
 
-      until lines.first.empty?
+      until lines.empty? || lines.first.empty?
         picture += [lines.first]
         lines.slice!(0)
       end
@@ -32,7 +33,7 @@ class Puzzle
       @tiles[tile] = picture
     end
 
-    puts @tiles
+    #puts @tiles
 
     @tiles.each{|id, data|
       top = encode(data[0].chars)
@@ -40,24 +41,58 @@ class Puzzle
       bottom = encode(data[9].chars.reverse)
       left = encode(data.map{|line| line[0]}.reverse)
 
-      @edges[id] = [top, right, bottom, left]
+      @edges[id] = [top, right, bottom, left].map{|v| v.to_i(2)}
+      @flipped[id] = [top, left, bottom, right].map{|v| v.reverse.to_i(2)}
     }
 
-    puts @edges
+    #puts "edges #{@edges}"
+    #puts "flipped #{@flipped}"
 
-    @edges.each{|id, lst|
-      lst.map{|v| v.to_i(2)}.tap{|encoded| puts "#{id}  #{encoded}"}
-      lst.map{|v| v.reverse.to_i(2)}.tap{|encoded| puts "#{id}r #{encoded}"}
-    }
-
+    #occ = @edges.values.flatten.each_with_object(Hash.new(0)){|v, hash| hash[v] += 1}
+    #puts "occ #{occ}"
+    #occocc = occ.values.each_with_object(Hash.new(0)){|v, hash| hash[v] += 1}
+    #puts "occocc #{occocc}"
   end
 
   def encode(chars)
     chars.map{|c| c == '#' ? '1' : '0'}.join
-      .tap{|encoded| puts "chars #{chars} encoded #{encoded}"}
+      #.tap{|encoded| puts "chars #{chars} encoded #{encoded}"}
   end
 
   def multiply
-    0
+    
+    #puts "mult edges #{@edges}"
+    #puts "mult flipped #{@flipped}"
+
+    perm = [false, true].repeated_permutation(@edges.length).to_a
+                        .map{|flipped_array|
+                          flipped_array.map.with_index.with_object(Hash.new(0)){|(flipped, index), hash|
+                            hash[@tiles.keys[index]] = flipped ? @flipped[@tiles.keys[index]] : @edges[@tiles.keys[index]]
+                          }
+                        }#.tap{puts "perm #{_1}"}
+
+    #perm.select{|p|
+      #occ = p.values.flatten.each_with_object(Hash.new(0)){|v, hash| hash[v] += 1}#.values.each_with_object(Hash.new(0)){|v, hash| hash[v] += 1}
+      #puts "p #{p}"
+      #puts "occ #{occ}"
+      ##p.map{|tile, _| puts "tile #{tile}"}
+      #occ.select{|_, v| v == 1}.keys.length == Math.sqrt(@edges.length) * 4
+    #}.tap{|s| puts "selected #{s}"}
+
+    perm.map{|p|
+      occ = p.values.flatten.each_with_object(Hash.new(0)){|v, hash| hash[v] += 1}
+      #puts "p #{p}"
+      #puts "occ #{occ}"
+      #occ.select{|_, v| v == 1}.keys.length == Math.sqrt(@edges.length) * 4
+      p.each_with_object(Hash.new(0)){|(k, v), hash|
+        hash[k] = v.map{|x| occ[x]}
+      }
+    }.select{|comb|
+      #puts "comb #{comb.values.flatten.select{|occ| occ == 1}.length}"
+      comb.values.flatten.select{|occ| occ == 1}.length == Math.sqrt(@edges.length) * 4
+    }.map{|comb|
+      comb.select{|_, l| l.select{|v| v == 1}.length == 2}.keys.sort
+    }.uniq.flatten.tap{|s| puts "selected #{s}"}#.reduce(&:*)
+
   end
 end
