5b17bab7e018ec47d4d958152952c01d90e7cc9c
Player One <steve@darvs.net>
2024-12-07 21:28:27 -0500

    - Solved Part 1 of Day 5 of AoC 2024

--- 2024/2405.rb/features/cucumber.feature
+++ 2024/2405.rb/features/cucumber.feature
@@ -6,7 +6,6 @@ Feature: XXXXXXXXXXXXXXXXX
 		Given the values in file 'smol.in'
     Then the result is 143
 
-	# Scenario: Part 1 full
-	# 	Given the values in file 'full.in'
- #    Then the result is 0
-	#
+	Scenario: Part 1 full
+		Given the values in file 'full.in'
+    Then the result is 5087
--- 2024/2405.rb/lib/main.rb
+++ 2024/2405.rb/lib/main.rb
@@ -4,6 +4,7 @@
 class Program
   def initialize(list)
     @before_after = []
+    @biggar = Hash.new([])
     @sequence = []
     @manual = []
 
@@ -13,10 +14,15 @@ class Program
       @manual += [line.split(',').map(&:to_i)] if line.chars.include?(',')
     }
 
-    # p "before_after: #{@before_after}"
-    # p "manuals: #{@manual}"
+    p "before_after: #{@before_after}"
+    p "manual: #{@manual}"
+    # build_sequence
     #
-    build_sequence
+    @before_after.each{|pair|
+      a, b = *pair
+      @biggar[b] += [a]
+    }
+    p "biggar: #{@biggar}"
   end
 
   def self.from_file(filename)
@@ -24,36 +30,58 @@ class Program
       .map(&:strip))
   end
 
-  def build_sequence
-    after = @before_after.map{|pair| pair[1]}.to_set
-    p "after: #{after.to_a.sort}"
+  # def build_sequence
+  #   after = @before_after.map{|pair| pair[1]}.to_set
+  #   p "after: #{after.to_a.sort}"
+  #
+  #   # p "before_after: #{@before_after}"
+  #   while @before_after.length > 1
+  #     p "b_a_l: #{@before_after.length}"
+  #     before = @before_after.map{|pair| pair[0]}.to_set
+  #     p "before: #{before.to_a.sort}"
+  #
+  #     last = (after - before).to_a[0]
+  #     # p "last: #{last}"
+  #
+  #     after.delete(last)
+  #     @sequence += [last]
+  #     @before_after.reject!{|p| p[1] == last}
+  #   end
+  #
+  #   @sequence += @before_after.first.reverse
+  #   @sequence.reverse!
+  #
+  #   p "before_after: #{@before_after}"
+  #   p "sequence: #{@sequence}"
+  # end
 
-    # p "before_after: #{@before_after}"
-    while @before_after.length > 1
-      p "b_a_l: #{@before_after.length}"
-      before = @before_after.map{|pair| pair[0]}.to_set
-      p "before: #{before.to_a.sort}"
-
-      last = (after - before).to_a[0]
-      # p "last: #{last}"
-
-      after.delete(last)
-      @sequence += [last]
-      @before_after.reject!{|p| p[1] == last}
-    end
-
-    @sequence += @before_after.first.reverse
-    @sequence.reverse!
-
-    p "before_after: #{@before_after}"
-    p "sequence: #{@sequence}"
-  end
+  # def run
+  #   @manual.select{|man|
+  #     #p "man: #{man}"
+  #     man.map{|m| @sequence.find_index{|s| m == s}}
+  #       .each_cons(2).all?{|a, b| a <= b}
+  #   }.map{|arr| arr[arr.length / 2]}.sum
+  # end
 
   def run
-    @manual.select{|man|
-      #p "man: #{man}"
-      man.map{|m| @sequence.find_index{|s| m == s}}
-        .each_cons(2).all?{|a, b| a <= b}
-    }.map{|arr| arr[arr.length / 2]}.sum
+    @manual.select{|m|
+      check_line(m)
+    }.map{|list| list[list.length / 2]}.sum
+  end
+
+  def check_line(line)
+    len = line.length
+    (0..len - 2).each{|n|
+      (n + 1..len - 1).each{|m|
+        return false unless check_pair(line[n], line[m])
+      }
+    }
+    true
+  end
+
+  def check_pair(a, b)
+    #p "check_pair #{a} #{b}"
+    #p "biggar[#{b}] #{@biggar[b]}"
+    @biggar[b].include?(a)
   end
 end
